68542aa739e1b7298255dd24ea39193328f1f741
Max Köhler <max.koehler@dcs-fuerth.de>
2024-01-21 15:19:23 +0100

    Leistugnen V1

--- fuchs-baum/src/app/homepage/homepage.component.ts
+++ fuchs-baum/src/app/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { IntroComponent } from '../intro/intro.component';
+import { LeistungenComponent } from '../leistungen/leistungen.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [[HeaderComponent, IntroComponent]],
+  imports: [[HeaderComponent, IntroComponent, LeistungenComponent]],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
