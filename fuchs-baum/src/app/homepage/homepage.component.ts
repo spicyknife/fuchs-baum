917c74f6fc2d7c43757c217b8b9429dedd0669c8
Max Köhler <max.koehler@dcs-fuerth.de>
2024-01-21 18:31:08 +0100

    Leistungen + Footer

--- fuchs-baum/src/app/homepage/homepage.component.ts
+++ fuchs-baum/src/app/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { IntroComponent } from '../intro/intro.component';
 import { LeistungenComponent } from '../leistungen/leistungen.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [[HeaderComponent, IntroComponent, LeistungenComponent]],
+  imports: [[HeaderComponent, IntroComponent, LeistungenComponent, FooterComponent]],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
