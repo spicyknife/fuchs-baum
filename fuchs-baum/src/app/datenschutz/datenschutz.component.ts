60c8a5a50c9c2fac879239e54db74ae918d77d32
Max Köhler <max.koehler@dcs-fuerth.de>
2024-02-06 19:29:43 +0100

    Datenschutz & Impressum

--- fuchs-baum/src/app/datenschutz/datenschutz.component.ts
+++ fuchs-baum/src/app/datenschutz/datenschutz.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-datenschutz',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './datenschutz.component.html',
   styleUrl: './datenschutz.component.scss'
 })
--- fuchs-baum/src/app/impressum/impressum.component.ts
+++ fuchs-baum/src/app/impressum/impressum.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
