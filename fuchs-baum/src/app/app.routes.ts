d29bc130ed2f80b8a15d678b62cc510f5eb2423c
Max Köhler <max.koehler@dcs-fuerth.de>
2024-02-12 19:27:28 +0100

    routes

--- fuchs-baum/src/app/app.routes.ts
+++ fuchs-baum/src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'disabled'})],
   exports: [RouterModule]
 })
 
